577ab9c9ca38733f08d2cbf3d9a17e8fe2dde5f4
CMSUpdater <info@cmsupdater.com>
2009-07-23 11:46:25 +0100

    Imported TER version 1.0.0

---
+++ ext_emconf.php
@@ -0,0 +1,42 @@
+<?php
+/***************************************************************
+ * Extension Manager/Repository config file for ext "tq_seo".
+ *
+ * Auto generated from CMSUpdater
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'TEQneers SEO Enhancements',
+	'description' => 'Search Engine Optimization (SEO), Metatags and Pagetitel manipulations',
+	'category' => 'misc',
+	'shy' => 0,
+	'version' => '1.0.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Markus Blaschke',
+	'author_email' => 'info@teqneers.de',
+	'author_company' => 'TEQneers GmbH & Co. KG',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'constraints' => array (
+		'depends' => array (
+			'typo3' => '',
+			'php' => '',
+		),
+	),
+);
+
+?>
