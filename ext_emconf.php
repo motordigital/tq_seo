1ae24ad615367279e95b0fb72452f1c89483d799
CMSUpdater <info@cmsupdater.com>
2015-03-18 16:11:21 +0100

    Imported changes

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,15 +1,16 @@
 <?php
-/***************************************************************
- * Extension Manager/Repository config file for ext "tq_seo".
- *
- * Auto generated from CMSUpdater
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
+########################################################################
+# Extension Manager/Repository config file for ext: "tq_seo"
+#
+# Auto generated 07-01-2010 21:35
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
 	'title' => 'TEQneers SEO Enhancements',
 	'description' => 'Search Engine Optimization (SEO), Metatags and Pagetitel manipulations',
 	'category' => 'misc',
@@ -29,14 +30,17 @@ $EM_CONF[$_EXTKEY] = array (
 	'author' => 'Markus Blaschke',
 	'author_email' => 'info@teqneers.de',
 	'author_company' => 'TEQneers GmbH & Co. KG',
-	'CGLcompliance' => NULL,
-	'CGLcompliance_note' => NULL,
-	'constraints' => array (
-		'depends' => array (
-			'typo3' => '',
-			'php' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
 		),
 	),
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"72a4";s:10:"README.txt";s:4:"3649";s:21:"ext_conf_template.txt";s:4:"d41d";s:12:"ext_icon.gif";s:4:"6ce1";s:17:"ext_localconf.php";s:4:"2ebd";s:14:"ext_tables.php";s:4:"46ab";s:14:"ext_tables.sql";s:4:"7698";s:16:"locallang_db.xml";s:4:"3b4d";s:17:"locallang_tca.xml";s:4:"4ef7";s:28:"static/default/constants.txt";s:4:"fdbf";s:24:"static/default/setup.txt";s:4:"362e";s:14:"doc/manual.sxw";s:4:"1310";s:24:"lib/class.linkparser.php";s:4:"5c21";s:22:"lib/class.metatags.php";s:4:"e25a";s:24:"lib/class.pagefooter.php";s:4:"f50b";s:23:"lib/class.pagetitle.php";s:4:"c650";}',
 );
 
 ?>
